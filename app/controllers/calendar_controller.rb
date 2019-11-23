03aa80997e73f4cb7221b07e983faa21c18853ee
Mike Glaz <mikeglaz@yahoo.com>
2019-11-23 11:21:19 -0600

    grid system

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,5 +1,5 @@
 class CalendarController < ApplicationController
-  helper_method :days_in_month, :current_month, :previous_month
+  helper_method :days_in_month, :current_month, :current_month_date, :calculate_days_view
 
   YEAR = 2020
 
@@ -8,7 +8,7 @@ class CalendarController < ApplicationController
   end
 
   def days_in_month(month)
-    Date.new(YEAR, month, 1).end_of_month.day
+    DateTime.new(YEAR, month, 1).end_of_month.day
   end
 
   def current_month
@@ -16,8 +16,28 @@ class CalendarController < ApplicationController
   end
 
   def current_month_date
-    Date.current.beginning
+    DateTime.current.change(day: 1)
   end
+
+  def calculate_days_view
+    # 6 (Friday)
+    wday = DateTime.current.wday
+
+    # Oct 26
+    prev_month_date = DateTime.current.change(day: 1) - wday.days
+
+
+    @days_array = []
+    (prev_month_date..DateTime.current.end_of_month).each do |day|
+      @days_array << day.day
+    end
+
+    @days_array
+
+
+  end
+
+
 
   # def previous_month
   #   current_month
