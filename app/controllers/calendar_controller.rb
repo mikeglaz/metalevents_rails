cfabec199220c2d246a4c51127b98a4a36e0e33d
Mike Glaz <mikeglaz@yahoo.com>
2019-11-23 10:34:18 -0600

    working on calendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,5 +1,5 @@
 class CalendarController < ApplicationController
-  helper_method :days_in_month
+  helper_method :days_in_month, :current_month, :previous_month
 
   YEAR = 2020
 
@@ -10,5 +10,17 @@ class CalendarController < ApplicationController
   def days_in_month(month)
     Date.new(YEAR, month, 1).end_of_month.day
   end
+
+  def current_month
+    @current_month ||= Date.current.month
+  end
+
+  def current_month_date
+    Date.current.beginning
+  end
+
+  # def previous_month
+  #   current_month
+  # end
 
 end
--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,5 +1,3 @@
 module CalendarHelper
-  def current_month
-    @current_month ||= Date.current.month
-  end
+
 end
