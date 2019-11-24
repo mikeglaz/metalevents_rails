45e2ca15d9bf78206f1703306f2740a06f750719
Mike Glaz <mikeglaz@yahoo.com>
2019-11-24 17:12:54 -0600

    calendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,7 +1,7 @@
 class CalendarController < ApplicationController
   helper_method :days_in_month, :current_month, :current_month_date, :calculate_days_view
 
-  YEAR = 2020
+  YEAR = 2019
 
   def show
 
@@ -21,15 +21,31 @@ class CalendarController < ApplicationController
 
   def calculate_days_view
     # 6 (Friday)
-    wday = DateTime.current.wday
+    first_day_of_week = DateTime.current.change(day: 1).wday
+    last_day_of_week = DateTime.current.end_of_month.wday
 
+    start_date = DateTime.current.change(day: 1)
+    end_date = DateTime.current.end_of_month
+
+    if first_day_of_week > 0
+      start_date -= first_day_of_week.days
+    end
+
+    if last_day_of_week < 6
+      end_date += (6-last_day_of_week).days
+    end
     # Oct 26
-    prev_month_date = DateTime.current.change(day: 1) - wday.days
+    prev_month_date = DateTime.current.change(day: 1) - first_day_of_week.days
+
+
+    next_month_date = DateTime.current.next_month.change(day: 1) + (6-last_day_of_week)
+
 
 
     @days_array = []
-    (prev_month_date..DateTime.current.end_of_month).each do |day|
-      @days_array << day.day
+
+    (start_date..end_date).each do |d|
+      @days_array << d.day
     end
 
     @days_array
