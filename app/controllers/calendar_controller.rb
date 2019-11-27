ad74653a226063922c04273b153e26a84ef2edab
Mike Glaz <mikeglaz@yahoo.com>
2019-11-27 17:08:31 -0600

    month advance working

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,5 +1,5 @@
 class CalendarController < ApplicationController
-  helper_method :days_in_month, :current_month, :current_month_date, :calculate_days_view
+  helper_method :days_in_month, :current_month, :current_month_date, :calculate_days_view, :select_month
 
   YEAR = 2019
 
@@ -15,17 +15,26 @@ class CalendarController < ApplicationController
     @current_month ||= Date.current.month
   end
 
+  def select_month(month_num)
+    @selected_month_date = DateTime.new(YEAR, month_num, 1)
+  end
+
   def current_month_date
     DateTime.current.change(day: 1)
   end
 
   def calculate_days_view
+    if params[:month].nil?
+      select_month(11)
+    else
+      select_month(params[:month].to_i)
+    end
     # 6 (Friday)
-    first_day_of_week = DateTime.current.change(day: 1).wday
-    last_day_of_week = DateTime.current.end_of_month.wday
+    first_day_of_week = @selected_month_date.wday
+    last_day_of_week = @selected_month_date.end_of_month.wday
 
-    start_date = DateTime.current.change(day: 1)
-    end_date = DateTime.current.end_of_month
+    start_date = @selected_month_date.change(day: 1)
+    end_date = @selected_month_date.end_of_month
 
     if first_day_of_week > 0
       start_date -= first_day_of_week.days
@@ -35,10 +44,10 @@ class CalendarController < ApplicationController
       end_date += (6-last_day_of_week).days
     end
     # Oct 26
-    prev_month_date = DateTime.current.change(day: 1) - first_day_of_week.days
+    prev_month_date = @selected_month_date.change(day: 1) - first_day_of_week.days
 
 
-    next_month_date = DateTime.current.next_month.change(day: 1) + (6-last_day_of_week)
+    next_month_date = @selected_month_date.next_month.change(day: 1) + (6-last_day_of_week)
 
 
 
