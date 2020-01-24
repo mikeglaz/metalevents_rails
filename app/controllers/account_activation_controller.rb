27d7cb971283af69ead579d79ef77ae73c8afdaa
Mike Glaz <mikeglaz@yahoo.com>
2020-01-23 19:05:43 -0600

    do not sign in after activation

--- app/controllers/account_activation_controller.rb
+++ app/controllers/account_activation_controller.rb
@@ -4,9 +4,9 @@ class AccountActivationController < ApplicationController
 
     if user && user.authenticated?(:activation_digest, params[:token])
       user.update_attribute(:activated, true)
-      sign_in(user)
-      flash[:success] = "Your account has been activated!"
-      redirect_to user
+      # sign_in(user)
+      flash[:success] = "Your account has been activated! You may now log in."
+      redirect_to root_path
     else
       flash[:danger] = "Invalid activation token"
       redirect_to root_path
